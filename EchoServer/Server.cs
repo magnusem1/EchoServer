46475ae50bde3a2558224ea2f8432f7840f32514
Magnus <Magnus@LAPTOP-J27TRD47>
2017-08-29 12:46:06 +0200

    EchoServer

---
+++ EchoServer/Server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoServer
+{
+    public class Server
+    {
+        public Server()
+        {
+
+        }
+
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Loopback, 7);
+            server.Start();
+
+            using (TcpClient client = server.AcceptTcpClient())
+            using (NetworkStream ns = client.GetStream())
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+                string inline = sr.ReadLine();
+                sw.WriteLine(inline);
+                sw.Flush();
+            }
+
+            
+
+        }
+    }
+}
